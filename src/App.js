e4a9bdcd108a37e6869b8285d4a92d22823c392c
Dinesh Sahu <masterking008.com>
2024-07-13 19:50:36 +0530

    notesapp

--- src/App.js
+++ src/App.js
@@ -5,20 +5,39 @@ import NoteGrid from "./components/NoteGrid";
 import AddNote from "./components/AddNote";
 
 function App() {
+  function myFunction() {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+  }
   return (
     <>
       <NoteProvider>
-        <body data-bs-theme="dark">
+        <body data-bs-theme="light">
           <nav className="navbar bg-body-tertiary">
             <div className="container-fluid">
               <a className="navbar-brand" href="#">
                 <h2>Notes</h2>
               </a>
+              <div className="form-check form-switch mx-4">
+                DARK MODE
+                <input
+                  className="form-check-input p-2 "
+                  type="checkbox"
+                  role="switch"
+                  id="flexSwitchCheckChecked"
+                  onclick={() => myFunction()}
+                />
+              </div>
             </div>
           </nav>
 
           <AddNote />
           <NoteGrid />
+          <footer className="py-5 text-center text-body-secondary bg-body-tertiary">
+            <p>Made by Dinesh</p>
+            <p className="mb-0">
+              <a href="#">Back to top</a>
+            </p>
+          </footer>
         </body>
       </NoteProvider>
     </>
--- src/components/NoteGrid.jsx
+++ src/components/NoteGrid.jsx
@@ -6,10 +6,14 @@ function NoteGrid() {
   const [notes, setNotes] = useContext(NoteContext);
   return (
     <>
-      <div className="m-5 d-flex flex-wrap min-vh-100">
-        {notes.map((note) => (
-          <NoteCard title={note.title} text={note.text} key={note.index} />
-        ))}
+      <div className="d-flex flex-wrap min-vh-100">
+        {notes.length > 0 ? (
+          notes.map((note) => (
+            <NoteCard title={note.title} text={note.text} key={note.index} />
+          ))
+        ) : (
+          <div className="container">No Note added</div>
+        )}
       </div>
     </>
   );
